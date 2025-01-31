0126dd73fae4ebca2f2d0d6c664076818019b931
unknown <akashprinceakash9986@gmail.com>
2025-01-31 22:46:27 +0530

    Added toggling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,25 @@
 import React, { useState } from 'react';
 
 export default function Header(){
-  // const [istoggle , settoggle]= useState(false);
-  //   const handletoggle=()=>{
-  //     settoggle(!istoggle)
-  //   }
+  const [istoggle , settoggle]= useState(true);
+    const handletoggle=()=>{
+      settoggle(!istoggle)
+    }
     return(
       <>
-      {/* <div onClick={handletoggle} className='block sm:hidden'>☰</div> */}
-      <header className=' text-white p-4 flex   flex-col  sm:flex-row justify-between text-lg '>
-       <h1 className=' text-4xl'>Akash's Portfolio</h1>
-      <nav className=' text-xl flex flex-col justify-around sm:w-1/2 sm:flex-row items-center'>
-        <a href='#home'>Home</a>
+      <header className=' text-white p-4 flex flex-col sm:flex-row justify-between text-lg '>
+      <div className='flex  justify-between mb-2'>
+      <h1 className='text-2xl sm:text-4xl inline-block'>My Portfolio</h1>
+        <div onClick={handletoggle} className='inline sm:hidden '>☰</div>
+       </div>
+    {istoggle && <nav className={` text-xl flex flex-row justify-around sm:w-1/2  items-center`}>
+        <a href='#home' >Home</a>
         <a href='#about'>About</a>
         {/* <a href='#service'>Services</a> */}
         <a href='#skill'>Skills</a>
         {/* <a href='#project'>Project</a> */}
         <a href='#contact'>contact</a>
-      </nav>
+      </nav>}
     </header>
       </>
     )
