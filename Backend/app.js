32b1923a14babb066a4177ea720f116f659d98e3
unknown <akashprinceakash9986@gmail.com>
2025-01-31 20:16:08 +0530

    implemented form submitting logic 2

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,6 @@ app.use(cors());
 const routes=require('./Router/index');
 app.use('/',routes)
 app.use(bodyParser.urlencoded({ extended: true }));
-// mongoose.connect('mongodb+srv://akashprinceakash9986:Akash2002@cluster0.cdabd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 app.listen(Port,(error) =>{
     if(error){
         console.log("failed to to server");     
@@ -19,12 +18,3 @@ app.listen(Port,(error) =>{
     console.log("Server is running at port number 8000");
     }
 })
-// .then((res)=>{
-//     console.log("MongoDB Connected")
-// })
-// .catch((error) =>{
-//      console.log("failed to connect "+error)
-// })
-// app.listen(Port, () => {
-//     console.log("Server is running at port number 8000");
-// });
